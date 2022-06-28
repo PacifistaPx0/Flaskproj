428dc600f407fe30c325582f02a411ef794fbbb4
PacifistaPx0 <richieadigwe@gmail.com>
2022-06-28 11:36:31 +0100

    Initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask, render_template, url_for
+
+def create_app(test_config=None):
+    #create and config the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE = os.path.join(app.instance_path, 'flask.sqlite'),
+    )
+    if test_config is None:
+        #load the instance config, if it exists, when not testing
+        app.config.from_mapping(test_config)
+    else:
+        #load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    #ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        return render_template('index.html')
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
