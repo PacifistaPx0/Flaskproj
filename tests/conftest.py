db41f0f28534ccfd0949bac99e6b15a58424e5df
PacifistaPx0 <richieadigwe@gmail.com>
2022-07-15 14:31:51 +0100

    changed app name

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 
 import pytest
-from flaskr import create_app
+from flaskr.app import create_app
 from flaskr.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
